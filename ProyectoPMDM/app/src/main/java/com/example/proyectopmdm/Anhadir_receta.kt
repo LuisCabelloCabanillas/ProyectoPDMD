f1eda3fc5647549afcefc07d2dccfd30761afdf3
Luis Cabello <luisitocabcabanillas@gmail.com>
2025-11-24 10:52:19 +0100

    Datos en .kt que no son importantes

--- ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Anhadir_receta.kt
+++ ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Anhadir_receta.kt
@@ -11,10 +11,5 @@ class Anhadir_receta : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_anhadir_receta)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/PantallaInicioApp.kt
+++ ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/PantallaInicioApp.kt
@@ -11,10 +11,5 @@ class PantallaInicioApp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pantalla_inicio_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
