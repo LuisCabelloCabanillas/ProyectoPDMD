4f872d0d76d893ea1e6f31cc6d69f44a77fb746b
Luis Cabello <luisitocabcabanillas@gmail.com>
2025-11-26 12:48:24 +0100

    .kt de registrar e iniciar sesion hechos con firebase auth terminado

--- ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Inicio_sesion.kt
+++ ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Inicio_sesion.kt
@@ -1,16 +1,71 @@
 package com.example.proyectopmdm
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Inicio_sesion : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_inicio_sesion)
 
+        auth = FirebaseAuth.getInstance()
+
+        val campoEmail = findViewById<EditText>(R.id.edtCorreo)
+        val campoContra = findViewById<EditText>(R.id.edtContra)
+        val btnInsesi = findViewById<Button>(R.id.btnInSe)
+
+        btnInsesi.setOnClickListener {
+            val email = campoEmail.text.toString().trim()
+            val contrasenha = campoContra.text.toString().trim()
+
+            if (validarCorreoYContrasenha(email,contrasenha)){
+                iniciarSesion(email,contrasenha)
+            }
+        }
     }
+
+    private fun validarCorreoYContrasenha(email: String, contrasenha: String): Boolean{
+        if (email.isEmpty()){
+            Toast.makeText(this, "Ingresa un email", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Ingresa un email válido", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (contrasenha.isEmpty()){
+            Toast.makeText(this, "Ingresa una contraseña", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (contrasenha.length < 6) {
+            Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun iniciarSesion(email: String,contrasenha: String){
+        auth.signInWithEmailAndPassword(email, contrasenha)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this, "Inicio de sesión correcto", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, PantallaInicioApp::class.java))
+                } else {
+                    Toast.makeText(this, "Contraseña o Correo INCORRECTO",
+                        Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
 }
--- ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Registrarse.kt
+++ ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Registrarse.kt
@@ -2,24 +2,76 @@ package com.example.proyectopmdm
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import org.intellij.lang.annotations.Pattern
 
 class Registrarse : AppCompatActivity() {
-    
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_registrarse)
 
-        val botonRegis = findViewById<Button>(R.id.btnInSe)
+        auth = FirebaseAuth.getInstance()
+
+        val emailTexto = findViewById<EditText>(R.id.edtCorreoRe)
+        val contraTexto = findViewById<EditText>(R.id.edtContraRe)
+        val botonRegis = findViewById<Button>(R.id.btnRegi)
 
         botonRegis.setOnClickListener {
-            startActivity(Intent(this, PantallaInicioApp::class.java))
+            val email = emailTexto.text.toString().trim()
+            val contrasenha = contraTexto.text.toString().trim()
+
+            if (validarEmailYContrasenha(email,contrasenha)){
+                createCuenta(email, contrasenha)
+            }
+
         }
 
+    }
+
+    private fun validarEmailYContrasenha(email: String, contrasenha: String): Boolean{
+        if (email.isEmpty()){
+            Toast.makeText(this, "Ingresa un email", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Ingresa un email válido", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (contrasenha.isEmpty()){
+            Toast.makeText(this, "Ingresa una contraseña", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (contrasenha.length < 6) {
+            Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun createCuenta(email: String, contrasenha: String){
+        auth.createUserWithEmailAndPassword(email, contrasenha)
+            .addOnCompleteListener(this) { task ->
+                if ( task.isSuccessful){
+                    Toast.makeText(this, "Cuenta creada correctamente", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, PantallaInicioApp::class.java))
+                } else {
+                    if (task.exception is FirebaseAuthUserCollisionException){
+                        Toast.makeText(this, "El email ya está en uso", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Error al crear la cuenta: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 }
