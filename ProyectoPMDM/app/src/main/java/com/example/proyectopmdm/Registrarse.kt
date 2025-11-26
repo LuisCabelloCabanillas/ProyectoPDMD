6c22e458f3bd3b691b777a38fd2b78592023762a
Luis Cabello <luisitocabcabanillas@gmail.com>
2025-11-26 14:14:28 +0100

    Archivos

--- ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Registrarse.kt
+++ ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Registrarse.kt
@@ -9,7 +9,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import org.intellij.lang.annotations.Pattern
 
 class Registrarse : AppCompatActivity() {
 
