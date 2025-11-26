531563d3d095d1d47f3cbbcba062d1cf27ddc5bf
Jesus <jvicentelopez@safareyes.es>
2025-11-26 09:28:54 +0100

    Merge Funcional

--- ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Registrarse.kt
+++ ProyectoPMDM/app/src/main/java/com/example/proyectopmdm/Registrarse.kt
@@ -1,6 +1,8 @@
 package com.example.proyectopmdm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,12 @@ class Registrarse : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_registrarse)
+
+        val botonRegis = findViewById<Button>(R.id.btnInSe)
+
+        botonRegis.setOnClickListener {
+            startActivity(Intent(this, PantallaInicioApp::class.java))
+        }
+
     }
 }
